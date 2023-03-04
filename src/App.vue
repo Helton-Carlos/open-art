04c3ab75c0125ffd6f429e8f3e38bbdc9d915d59
Helton Brito <heltoncarlospa@hotmail.com>
2023-03-04 14:11:52 -0300

    feat: add vue router 4

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { useRouter } from "vue-router";
+
+const router = useRouter()
+
+function homeRouter() {
+  router.push({ name: "home" });
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="homeRouter">home</button>
+    <RouterView />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="card">
+    <p>
+      Home
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
