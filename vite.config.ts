b4b2fa6086c43817ed53989cb4808b6c79afa7f3
Helton Brito <heltoncarlospa@hotmail.com>
2023-03-06 22:23:31 -0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueTypeImports from "vite-plugin-vue-type-imports";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueTypeImports()],
+  plugins: [vue(), VueTypeImports(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: [
       { find: "@/", replacement: "/src" },
