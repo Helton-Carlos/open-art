9e460f3cabea396a22418e147b5108a183527607
Helton Brito <heltoncarlospa@hotmail.com>
2023-03-11 16:46:05 -0300

    feat: add component Menu

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import Button from "../Button/Button.vue";
+
+defineEmits<{ (e: "onClick"): void }>();
+
+function test() {
+  alert("path")
+}
+
+const router = [
+  { route: "About OpenArt" },
+  { route: "Blog" },
+  { route: "Help" },
+  { route: "Contact" },
+];
+</script>
+
+<template>
+  <div class="menu">
+    <div class="w-11/12 mx-auto">
+
+      <div class="flex items-center justify-between pb-8">
+        <img src="@/assets/navbar/Logo.svg" alt="Logo" />
+        <img
+          class="cursor-pointer" 
+          src="@/assets/navbar/Close.svg"
+          alt="Menu"
+          @click="$emit('onClick')"
+        />
+      </div>
+
+      <div
+        class="flex justify-center pb-5 hover:text-primary"
+        v-for="path in router"
+      >
+        <p class="font-extrabold">{{ path.route }}</p>
+      </div>
+
+      <div class="flex justify-center">
+        <Button title="Connect wallet" color="standard" :onClick="test" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import Menu from "../Menu/Menu.vue";
+
+const toggleMenu = ref<boolean>(false);
+
+function openMenu() {
+  toggleMenu.value = !toggleMenu.value;
+}
+
+function test() {
+  toggleMenu.value = false;
+}
 </script>
 
 <template>
+  <Menu v-show="toggleMenu" :onClick="test" />
   <div class="w-11/12 mx-auto mt-4 flex justify-between">
-   <img src="@/assets/navbar/Logo.svg" alt="Logo" />
-   <img src="@/assets/navbar/Menu.svg" alt="Menu" />
+    <img src="@/assets/navbar/Logo.svg" alt="Logo" />
+    <img
+      class="cursor-pointer"
+      src="@/assets/navbar/Menu.svg"
+      alt="Menu"
+      @click="openMenu"
+    />
   </div>
 </template>
