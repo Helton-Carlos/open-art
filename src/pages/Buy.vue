7a513a3dd1cd20389f8d1cf01dc0739e910ee3a5
Helton Brito <heltoncarlospa@hotmail.com>
2023-03-23 21:36:17 -0300

    fix: add get cards

--- src/pages/Buy.vue
+++ src/pages/Buy.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import cards from "@/utils/utils-card";
 
-const { id } = defineProps<{ id: number }>();
+const { id } = defineProps<{ id: number | string }>();
+
+const getCard = computed(() => {
+  return cards.find((item) => item.id == id);
+});
 </script>
 
 <template>
   <div class="w-11/12 my-4 mx-auto text-center">
-    {{ id }}
+    <hr>
+    <div class="flex justify-between items-center">
+      <div class="flex my-2">
+        <div class="text-left pl-4">
+          <p class="text-gray-dark font-extrabold text-md">
+            {{ getCard.title }}
+          </p>
+          <p class="text-gray font-semibold text-sm">
+            {{ getCard.persona }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
