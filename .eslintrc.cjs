234e6398dea0fcab384530d3d3acec8455fee536
Helton Brito <heltoncarlospa@hotmail.com>
2023-04-03 22:39:00 -0300

    feat: add eslintrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vitest-globals/recommended',
+  ],
+  env: {
+    'vitest-globals/env': true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  globals: {
+    __dirname: true,
+    process: true,
+  },
+};
