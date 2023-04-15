b6640ee455e5b8d16409b3e9ab2fe3e96cc24942
Helton Brito <heltoncarlospa@hotmail.com>
2023-04-15 16:01:40 -0300

    feat: add contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,11 +6,18 @@ const route = useRoute();
 
 const title = route.meta.title;
 const subTitle = route.meta.subTitle;
-
 </script>
 
 <template>
   <div class="containner my-4 text-center">
     <Title :title="title" :subTitle="subTitle" />
+    <div class="text-left card ">
+      <h4 class="font-bold text-base py-4">Contact us</h4>
+      <p>Rua Sete de Setembro, 1329</p>
+      <p>Vila Estádio - Presidente Prudente, SP</p>
+      <p>openart@gmail.com</p>
+      <p>Telefone: 18 3903-9771</p>
+      <p>Whatsapp: 18 99745-1107</p>
+    </div>
   </div>
 </template>
--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,16 +1,48 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
 import Title from "@/components/Title/Title.vue";
+import InputSearc from "@/components/InputSearc/InputSearc.vue";
+import Button from "@/components/Button/Button.vue";
 
 const route = useRoute();
 
 const title = route.meta.title;
 const subTitle = route.meta.subTitle;
 
+const email = ref<string>("");
+const text = ref<string>("");
+
+function registerCustomer() {
+  const registerCustomer = {
+    name: text.value,
+    email: email.value,
+  };
+
+  console.log(registerCustomer);
+}
 </script>
 
 <template>
-  <div class="containner my-4 text-center">
+  <div class="my-4 containner text-center">
     <Title :title="title" :subTitle="subTitle" />
+
+    <form @submit.prevent="registerCustomer">
+      <h4 class="font-bold text-base">Put your question.</h4>
+
+      <InputSearc
+        placeholder="Type your e-mail..."
+        type="e-mail"
+        v-model:modelValue="email"
+      />
+
+      <InputSearc
+        placeholder="Type your question..."
+        type="text"
+        v-model:modelValue="text"
+      />
+
+      <Button title="Submit question" color="standard" />
+    </form>
   </div>
 </template>
