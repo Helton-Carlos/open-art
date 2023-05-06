5584107691294c85311cb848ae483f81d639e875
Helton Brito <heltoncarlospa@hotmail.com>
2023-05-06 18:23:44 -0300

    feat: created user profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -4,11 +4,6 @@ import { useRoute } from "vue-router";
 import { useUserStore } from "@/store/user";
 import Title from "@/components/Title/Title.vue";
 
-interface UserInfo {
-  name: string;
-  email: string;
-}
-
 const route = useRoute();
 
 const user = useUserStore();
@@ -16,8 +11,8 @@ const user = useUserStore();
 const title = route.meta.title;
 const subTitle = route.meta.subTitle;
 
-const users = computed<UserInfo>(() => {
-  return user.userStorage();
+const users = computed<any>(() => {
+  return user.users;
 });
 </script>
 
@@ -28,14 +23,66 @@ const users = computed<UserInfo>(() => {
     <div class="flex justify-center items-center">
       <div class="text-center">
         <img
-          src="@/assets/profile/persona.jpg"
+          :src="users.image"
           alt="persona"
           class="w-[90px] h-[90px] rounded-full mx-auto"
         />
 
-        <h2 class="pt-4 font-bold">{{ users.name }}</h2>
+        <h2 class="pt-4 font-bold capitalize">{{ users.name }}</h2>
         <p class="font-bold text-gray">{{ users.email }}</p>
+
+        <div class="flex items-center mt-4">
+          <p class="font-bold">Followed by</p>
+          <div class="flex flex-row-reverse md:mt-2 lg:mt-0">
+            <span
+              class="z-0 ml-px inline-flex h-8 w-8 items-center justify-center rounded-full border-2 border-white bg-[#E0E5F2] text-xs text-navy-700"
+              >{{ users.followers }}</span
+            ><span
+              class="z-10 -mr-3 h-8 w-8 rounded-full border-2 border-white"
+            >
+              <img
+                class="h-full w-full rounded-full object-cover"
+                src="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/avatar1.eeef2af6dfcd3ff23cb8.png"
+                alt="person"
+                data-testid="imagePersona-id"
+              />
+            </span>
+            <span class="z-10 -mr-3 h-8 w-8 rounded-full border-2 border-white">
+              <img
+                class="h-full w-full rounded-full object-cover"
+                src="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/avatar2.5692c39db4f8c0ea999e.png"
+                alt="person"
+              />
+            </span>
+            <span class="z-10 -mr-3 h-8 w-8 rounded-full border-2 border-white">
+              <img
+                class="h-full w-full rounded-full object-cover"
+                src="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/avatar3.9f646ac5920fa40adf00.png"
+                alt="person"
+              />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
+
+    <div class="w-4/5 flex justify-between mx-auto pt-4 text-left">
+      <div>
+        <p class="text-gray-dark font-bold text-xl">{{ users.following }}</p>
+        <h4 class="text-gray">Followers</h4>
+      </div>
+
+      <div>
+        <p class="text-gray-dark font-bold text-xl">{{ users.followers }}</p>
+        <h4 class="text-gray">Followers</h4>
+      </div>
+    </div>
+
+    <div class="w-4/5 mx-auto pt-4 text-left">
+      <p class="text-gray-dark font-bold text-xl">Who am I:</p>
+      <p class="text-gray">{{ users.context }}</p>
+    </div>
+
+    <p class="text-gray-dark my-8">Member since {{ users.since }}</p>
   </div>
 </template>
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,11 +1,44 @@
 import { defineStore } from "pinia";
 
+interface State {
+  users: UserInfo | null;
+}
+
 export const useUserStore = defineStore("user", {
+  state: (): State => {
+    return {
+      users: {
+        name: "helton",
+        email: "helton@gmail.com",
+        money: "200.00",
+        following: 252,
+        followers: 92,
+        image: "src/assets/profile/persona.jpg",
+        since: 2021,
+        context:
+          "Helton Brito is a multi-disciplinary artist exploring analog + digital realms since 1990. Collaborators inc July, BMW, Comme Des Garçons, ICA, NTS, Sonos,  Stone Island, Tate Modern + Warp.",
+      },
+    };
+  },
   actions: {
     userStorage() {
-      const state = JSON.parse(localStorage.getItem("user"));
-      return state
+      const logar = JSON.parse(localStorage.getItem("user"));
+      if (
+        logar.email === this.users?.email &&
+        logar.name === this.users?.name
+      ) {
+        return this.users;
+      }
     },
   },
 });
-
+interface UserInfo {
+  name: string;
+  email: string;
+  money: string;
+  following: number;
+  followers: number;
+  image: string;
+  since: number;
+  context: string;
+}
